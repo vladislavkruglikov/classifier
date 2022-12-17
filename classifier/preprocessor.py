863df9a776aa4d13b5c628db7bc721fa624917b9
Vladislav Kruglikov <lildatascientist@gmail.com>
2022-12-18 00:11:40 +0300

    add stopwords

--- classifier/preprocessor.py
+++ classifier/preprocessor.py
@@ -1,10 +1,15 @@
+import nltk
 import string
 
 from typing import List
 
 
+nltk.download('stopwords')
+
+
 class Preprocessor:
-    def __init__(self) -> None:
+    def __init__(self, remove_stop_words: bool = False) -> None:
+        self._stem = stem
         self._version = "0.0.2"
         self._char_mapping = {
             "e": "е",
@@ -13,7 +18,9 @@ class Preprocessor:
             "c": "с",
             "a": "а",
         }
+        self._remove_stop_words = remove_stop_words
         self._special_symbols = string.punctuation + "-"
+        self._stopwords = set(nltk.corpus.stopwords.words(["russian", "english"]))
 
     def _apply_char_mapping(self, text: str) -> str:
         for char, replacement in self._char_mapping.items():
@@ -28,6 +35,9 @@ class Preprocessor:
         tokens = text.split()
         tokens = [token.strip(self._special_symbols) for token in tokens]
         tokens = [token for token in tokens if not token.isdigit()]
+
+        if self._remove_stop_words:
+            tokens = [token for token in tokens if token not in self._stopwords]
 
         text = " ".join(tokens)
 
